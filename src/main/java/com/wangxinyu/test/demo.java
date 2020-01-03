4d0b3d46d5024a6c79dcf6de9163773f135188d2
Jane1220 <1521609483@qq.com>
2020-01-03 08:08:43 +0800

    项目初始

---
+++ src/main/java/com/wangxinyu/test/demo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wangxinyu.test;
+
+/**
+ * @作者： 王新宇
+ * @时间：2020年1月3日
+ */
+public class demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("helloWord");
+		
+	}
+
+}
